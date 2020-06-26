13ad9e904f09802fc16688d2679a65a6256570e9
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-06-26 20:17:07 +0500

    DARK THEME COMPONENT
    HERE I MADE A  REACT DARK THEME COMPONENT WITH COUNTER

---
+++ src/components/parents.js
@@ -0,0 +1,46 @@
+import React ,{ useState } from 'react'
+import Child from './child'
+import '../App.css'
+
+const Parent = () => {
+    let p = document.getElementById('p');
+    const [isLit, setLit] = useState(true);
+    let [age, setAge] = useState(24);
+    let [decrease , decreaseAge] = useState(age)
+  
+    const lightUpdate = () => {
+        console.log('button click');
+        setLit(!isLit);  
+  
+    
+    }
+  
+    const ageIncrease = () => {
+      console.log('button click');
+      setAge(++age);
+  }
+  
+  
+  const ageDecrease = () => {
+    console.log('button click');
+    decreaseAge(p = --age);
+  }
+  
+    return (
+        <div className={`room ${isLit? 'dark' : 'white'}`}>
+  
+              <p> this is shit {isLit? "light" : "dark"}</p> 
+                <br/>
+                <p id='p'>age : {age}</p>
+                {/* <p>age : {decrease}</p> */}
+  
+                <button onClick={() => {setLit(!isLit);  }}>For Light</button>
+                <button onClick={ageIncrease}>For Age</button>
+                <button onClick={ageDecrease}>For Decrease</button>
+  
+        </div>  
+    );
+    
+}
+
+export default Parent
